641cc7fdfe4f997c68932489ec2dd756d21afe30
Natalia Khasanova <natell_2005@mail.ru>
2023-09-25 12:33:58 +0300

    добавила решение задачи 13

--- Program.cs
+++ Program.cs
@@ -4,14 +4,41 @@
 // 918 -> 1
 
 
-Console.Write("Введите трехзначное число: ");
+// Console.Write("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int position2 = (number / 10 % 10);
+// if (number > 99 && number < 1000)
+// {
+//     Console.WriteLine($"Вторая цифра в числе {number} - это {position2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Введенное число {number} - не является трехзначным");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите  число от 0 до 9999: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int position2 = (number / 10 % 10);
+int position3 = (number / 10 % 10);
+int position4 = (number % 10);
+
 if (number > 99 && number < 1000)
 {
-    Console.WriteLine($"Вторая цифра в числе {number} - это {position2}");
+    Console.WriteLine($"Третья цифра в числе {number} - это {position4}");
+}
+if (number < 100)
+{
+    Console.WriteLine($"У введенного числа {number} - нет третьей цифры");
+}
+if (number > 999 && number < 9999)
+{
+    Console.WriteLine($"Третья цифра в числе {number} - это {position3}");
 }
 else
 {
-    Console.WriteLine($"Введенное число {number} - не является трехзначным");
+    Console.WriteLine($"Введенное число {number}  -  вне заданного диапазона");
 }
